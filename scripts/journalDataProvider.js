ac44186fa698dbfe63720dc1df88d3dbc42913a6
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2020-12-09 11:21:47 -0600

    Save journal button is now functional and sends data to the API

--- scripts/journalDataProvider.js
+++ scripts/journalDataProvider.js
@@ -1,14 +1,8 @@
 // Selectors
-const eventHub = document.querySelector(".form-container")
+const eventHub = document.querySelector(".container")
 
 // This will hold the API data locally
 let journal = []
-
-const dispatchStateChangeEvent = () => {
-    const entryStateChangeEvent = new CustomEvent("entryStateChange");
-
-    eventHub.dispatchEvent(entryStateChangeEvent);
-}
 
 // Fetch entries data from the API and then store the information on the local journal variable
 export const getEntries = () => {
@@ -20,13 +14,18 @@ export const getEntries = () => {
 // Returns a copy of the journal array
 export const useEntries = () => journal.slice();
 
-export const saveEntry = entry => {
+// This is called within the saveEntry function to let the eventHub know when a new entry has been made
+const dispatchStateChangeEvent = () => eventHub.dispatchEvent(new CustomEvent("journalStateChange"));
+
+
+// Called when a new entry is saved on the form
+export const saveJournalEntry = newJournalEntry => {
     return fetch('http://localhost:8088/entries', {
         method: "POST",
         headers: {
             "content-Type": "application/json"
         },
-        body: JSON.stringify(entry)
+        body: JSON.stringify(newJournalEntry)
     })
     .then(getEntries)
     .then(dispatchStateChangeEvent);
